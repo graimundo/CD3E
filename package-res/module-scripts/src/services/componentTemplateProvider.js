306e5165d8c0fc5158da9ee1a16996ef0df03fb2
Guilherme Raimundo <guilherme.raimundo@webdetails.pt>
2015-01-26 15:13:22 +0000

    - Repository bootstrap with files from the marketplace project

---
+++ package-res/module-scripts/src/services/componentTemplateProvider.js
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2002 - 2014 Webdetails, a Pentaho company.  All rights reserved.
+ *
+ * This software was developed by Webdetails and is provided under the terms
+ * of the Mozilla Public License, Version 2.0, or any later version. You may not use
+ * this file except in compliance with the license. If you need a copy of the license,
+ * please go to  http://mozilla.org/MPL/2.0/. The Initial Developer is Webdetails.
+ *
+ * Software distributed under the Mozilla Public License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or  implied. Please refer to
+ * the license for the specific language governing your rights and limitations.
+ */
+
+
+'use strict';
+
+define(
+    [
+        'marketplace',
+        'common-ui/underscore',
+        'Base'
+    ],
+    function ( app, _, Base ) {
+      console.log( "Required services/componentTemplateProvider.js" );
+
+      var service = app.factory( 'componentTemplateProvider',
+          [ '$translate',
+            function ( $translate ) {
+
+                var componentTemplateProvider = Base.extend(
+                    {
+                        /// Specify service here
+                        getComponentTemplates: undefined
+                    },
+                    {
+                        /// Specify static stuff here
+                    }
+                );
+
+                return componentTemplateProvider;
+            }
+          ]);
+
+      return service;
+    }
+);
+
