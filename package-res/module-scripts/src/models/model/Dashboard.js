5ac42a83e229a0c805f2fe124d46b39ba7c39046
Diogo Mariano <diogo.mariano@webdetails.pt>
2015-01-28 18:12:03 +0000

    - Add rootElement add block for elements different than Rows

--- package-res/module-scripts/src/models/model/Dashboard.js
+++ package-res/module-scripts/src/models/model/Dashboard.js
@@ -34,7 +34,12 @@ define(['Base', 'common-ui/underscore'], function(Base, _){
      * @param RowLayoutElement rootElement
      */
     addRootElement: function( /*RowLayoutElement*/rootElement ) {
-      this._rootElements.push( rootElement );
+      if( rootElement instanceof RowLayoutElement ) {
+        this._rootElements.push( rootElement );
+      } else {
+        console.log( "Adding element different than RowLayoutElement. Blocking");
+      }
+      
       return this;
     },
 
